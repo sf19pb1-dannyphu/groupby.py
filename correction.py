eb41379a34b4c0be8f8efcdef112aff493f6a458
Danny Phu <54083130+dannyphu@users.noreply.github.com>
2019-10-15 20:01:48 -0400

    Create correction.py

---
+++ correction.py
@@ -0,0 +1,15 @@
+import pandas as pd
+ 
+df = pd.read_csv('https://data.cityofnewyork.us/api/views/rsgh-akpg/rows.csv?accessType=DOWNLOAD')
+
+#line 1
+series = df.groupby('Borough').count()
+
+#line 2
+series2 = df.groupby('Borough')['Species'].count()
+
+print("Line 1 result:")
+print(series)
+print()
+print("Line 2 result:")
+print(series2)
