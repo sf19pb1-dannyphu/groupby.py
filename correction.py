e0bb43cfcc3085c2a25e073049cc22aef9e97516
Danny Phu <54083130+dannyphu@users.noreply.github.com>
2019-10-15 20:09:15 -0400

    Update correction.py

--- correction.py
+++ correction.py
@@ -14,8 +14,8 @@ series = df.groupby('Borough').count()
 #line 2
 series2 = df.groupby('Borough')['Species'].count()
 
-print("Line 1 result:")
+print("Line 1 result, all columns in csv:")
 print(series)
 print()
-print("Line 2 result:")
+print("Line 2 result, only counting "Species" column:")
 print(series2)
